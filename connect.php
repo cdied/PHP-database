1958b841707d97dbd88514e61e87e7d9a93493d0
Sayed M Rezaie <cdiedwbh@gmail.com>
2020-11-20 22:56:55 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 
 // database details
-$servername = "sql104.epizy.com"; // enter your server name
-$username = "epiz_27011933";      // enter your username
-$password = "xJNqUDKeg1";         // enter your password
-$dbname = "epiz_27011933_table";  // enter your databases name
+$servername = "myServer";     // enter your server name
+$username = "username";       // enter your username
+$password = "Password";       // enter your password
+$dbname = "myDatabase";       // enter your databases name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
